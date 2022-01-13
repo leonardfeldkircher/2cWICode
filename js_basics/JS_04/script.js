6c8dd1d671cf78ce5f975704f0807c342e1a7327
leonard <leonard@htldornbirn.at>
2022-01-13 14:13:58 +0100

    finished js04

---
+++ js_basics/JS_04/script.js
@@ -0,0 +1,27 @@
+for (let count = 7;count<46;count++){
+    console.log(count + " ")
+}
+
+for (let count = 9;count<100;count++){
+    console.log(count + " ")
+}
+
+for (let count = 10; count>=0; count--){
+    console.log(count + " ")
+}
+
+for (let count = 1234; count>=1207; count--){
+    console.log(count + " ")
+}
+
+for (let count = 0;count<10;count++){
+    console.log("Leonard")
+}
+
+for (let count = 0;count<10;count++){
+    console.log(count + " ")
+}
+    console.log("Leonard")
+for (let count = 10; count>=0; count--){
+    console.log(count + " ")
+}
