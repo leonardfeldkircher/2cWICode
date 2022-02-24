82afb32c58bb2827323cd827912c12ba6840b570
Your Name <you@example.com>
2022-02-24 14:23:50 +0100

    finished js07

--- js_basics/JS_06/script.js
+++ js_basics/JS_06/script.js
@@ -2,16 +2,16 @@ let random = Math.random() * 1000;
 let randomAsInteger = Math.round(random);
 console.log(randomAsInteger);
 
-if (randomAsInteger>800){
+if (randomAsInteger > 800) {
     console.log("Bigger then 800");
-    }
-    else if (randomAsInteger>500 && randomAsInteger<800){
-        console.log("Zwischen 500 und 800");
-        }
-        else if (randomAsInteger>200 && randomAsInteger<500){
-            console.log("Zwischen 200 und 500");
-            }
-            else{console.log("Kleiner als 200")
-
-            }
+}
+else if (randomAsInteger > 500 && randomAsInteger < 800) {
+    console.log("Zwischen 500 und 800");
+}
+else if (randomAsInteger > 200 && randomAsInteger < 500) {
+    console.log("Zwischen 200 und 500");
+}
+else {
+    console.log("Kleiner als 200")
+}
 
