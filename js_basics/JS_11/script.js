e4467f8ff2e601d9bf1ca1d1ac80dea7fc4ff547
leonard <you@example.com>
2022-04-28 17:03:30 +0200

    finished js09 and js11

---
+++ js_basics/JS_11/script.js
@@ -0,0 +1,25 @@
+var res = add(8,7);
+console.log(res);
+
+function add(a,b) {
+    var result = a + b;
+    return result; 
+}
+
+
+var res2 = subtract(6,2);
+console.log(res2);
+
+function subtract(a,b) {
+    var result2 = a / b;
+    return result2; 
+}
+
+
+var res3 = multiply(12,3);
+console.log(res3);
+
+function multiply(a,b) {
+    var result3 = a * b;
+    return result3; 
+}
